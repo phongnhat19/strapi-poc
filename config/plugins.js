0532a86a2d93045a082cce62bd6734b4283ee170
phongnhat19 <phongnhat19@gmail.com>
2021-01-31 02:52:59 +0700

    hot fix

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
     upload: {
         provider: 'cloudinary',
         providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
+            cloud_name: env('CLOUDINARY_NAME') || 'gophuot',
+            api_key: env('CLOUDINARY_KEY') || '269811349515945',
+            api_secret: env('CLOUDINARY_SECRET') || 'Rh8jRkl4m8z8a',
         },
     },
 });
