8ee363ab526265449135b2d9993a749999e444cc
phongnhat19 <phongnhat19@gmail.com>
2021-02-03 20:54:49 +0700

    hot fix

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -81,9 +81,11 @@ module.exports = {
             entity = await strapi.services.orders.create(body);
         }
 
-        await strapi.plugins['email'].services.email.sendTemplatedEmail({
-            to: 'nhat.nguyen@kardiachain.io'
-        }, emailTemplate, {order: entity});
+        if (body.mail) {
+            await strapi.plugins['email'].services.email.sendTemplatedEmail({
+                to: body.mail
+            }, emailTemplate, {order: entity});
+        }
 
         return sanitizeEntity(entity, { model: strapi.models.orders });
     },
